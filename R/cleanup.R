feb7b3bef82080f5afbd3b28512c5a89cd681471
Sebastian Kopf <seb.kopf@gmail.com>
2017-12-03 13:16:21 -0700

    import data cleanup functions from isoreader and readr

---
+++ R/cleanup.R
@@ -0,0 +1,22 @@
+# cleanup functions of data read from files
+
+#' @export
+isoreader::extract_substring
+
+#' @export
+isoreader::extract_word
+
+#' @export
+readr::parse_number
+
+#' @export
+readr::parse_logical
+
+#' @export
+readr::parse_integer
+
+#' @export
+readr::parse_double
+
+#' @export
+readr::parse_datetime
