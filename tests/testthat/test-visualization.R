2ad3c3419da7cf0a5d0b2007036b53c33515de19
Sebastian Kopf <seb.kopf@gmail.com>
2018-03-28 14:20:42 -0600

    fix visualization function call

--- tests/testthat/test-visualization.R
+++ tests/testthat/test-visualization.R
@@ -11,10 +11,10 @@ test_that("test that referencd peak visualization works", {
   expect_error(iso_plot_ref_peaks(ggplot2::mpg, is_ref_condition = cyl > 100, ratio = displ, group_id = model), "no data")
 
   # simple generation tests
-  expect_true((p <- iso_plot_ref_peaks(ggplot2::mpg, is_ref_condition = TRUE, ratio = displ, group_id = model)) %>% is.ggplot())
-  expect_equal(p$mapping, list(fill = sym("ref_peak_nr"), x = sym("model"), y = sym("total_delta_deviation")) %>% { class(.) <- "uneval"; . })
-  expect_true(iso_plot_ref_peaks(ggplot2::mpg, is_ref_condition = TRUE, ratio = c(displ, hwy), group_id = model) %>% is.ggplot())
-  expect_true(iso_plot_ref_peaks(ggplot2::mpg, is_ref_condition = cyl > 6, ratio = c(displ, hwy), group_id = model) %>% is.ggplot())
+  expect_true((p <- iso_plot_ref_peaks(ggplot2::mpg, is_ref_condition = TRUE, ratio = displ, group_id = model)) %>% ggplot2::is.ggplot())
+  expect_equal(p$mapping, list(fill = quo(ref_peak_nr), x = quo(model), y = quo(total_delta_deviation)) %>% { class(.) <- "uneval"; . })
+  expect_true(iso_plot_ref_peaks(ggplot2::mpg, is_ref_condition = TRUE, ratio = c(displ, hwy), group_id = model) %>% ggplot2::is.ggplot())
+  expect_true(iso_plot_ref_peaks(ggplot2::mpg, is_ref_condition = cyl > 6, ratio = c(displ, hwy), group_id = model) %>% ggplot2::is.ggplot())
 
   # FIXME: test more (evaluate the resulting plots in more detail?)
 
