56542a184cb2530b65b55e995de19cb1ec4b1bb4
Sebastian Kopf <seb.kopf@gmail.com>
2018-04-09 21:12:37 -0600

    update visualization function for calibration parameters

--- R/visualization.R
+++ R/visualization.R
@@ -55,45 +55,62 @@ iso_plot_ref_peaks <- function(dt, is_ref_condition, ratio = default(ratio), gro
   return(p)
 }
 
-#' Visualize results for delta calibration
-#' @inheritParams iso_print_data_table
-#' @param x the x axis for the calibration parameters, by default a datetime but can also work as text
+#' Visualize calibration parameters
+#'
+#' Visualize calibration coefficients and summary.
+#'
+#' @param x the x axis aesthetic for the calibration parameters, can be datetime, text or numeric
 #' @param include_from_summary which parameters from the fit summary to include
+#' @param color variable to use for color aesthetic for the plot
+#' @param shape variable to use for shape aesthetic for the plot
+#' @param size variable to use for size aesthetic for the plot
 #' @param date_breaks what breaks to use for the x axis if it is a datetime
 #' @param date_labels datetime label pattern for x axis if it is a datetime
 #' @export
-iso_visualize_delta_calib_fits <- function(dt, x = default(file_datetime), color = NULL, shape = NULL, size = NULL,
-                                           date_breaks = "2 hours", date_labels = "%d %b %H:%M",
-                                           include_from_summary = c(adj.r.squared, deviance)) {
+iso_plot_calibration_parameters <- function(dt, calibration = "", x,
+                                       include_from_summary = c(R2 = adj.r.squared, RMS = deviance),
+                                       color = NULL, shape = NULL, size = NULL,
+                                       date_breaks = "2 hours", date_labels = "%d %b %H:%M") {
 
   # safety checks
   if (missing(dt)) stop("no data table supplied", call. = FALSE)
-  if (length(missing <- setdiff(c("calib_delta_coefs", "calib_delta_summary"), names(dt))) > 0) {
-    glue("missing columns in data table: '{collapse(missing, \"', '\")}'. Make sure to run iso_calibrate_delta() first and keep these columns.") %>%
-      stop(call. = FALSE)
-  }
+  if (missing(x)) stop("no x axis aesthetic specified", call. = FALSE)
 
-  # pull out all coefficients (all for now, should always show all)
-  calib_coefs <- dt %>% select(-calib_delta_summary) %>%
-    iso_unnest_delta_calib_coefs(select = c(term, estimate, std.error))
+  # check for model parameters
+  calib_vars <- get_calibration_vars(calibration)
+  check_calibration_cols(!!enquo(dt), calib_vars$model_params)
 
-  # pull out summary
+  # pull out all coefficients (all for now, should always show all?)
+  calib_coefs <- dt %>%
+    iso_unnest_calibration_coefs(
+      calibration = calibration,
+      select = c(term, estimate, std.error),
+      keep_other_list_data = FALSE
+    )
+
+  # pull out requested summary
   select_quo <- enquo(include_from_summary)
-  calib_summary <- dt %>% select(-calib_delta_fit, -calib_delta_coefs) %>%
-    iso_unnest_delta_calib_summary(select = !!select_quo)
+  calib_summary <- dt %>%
+    iso_unnest_calibration_summary(
+      calibration = calibration,
+      select = everything(),
+      keep_other_list_data = FALSE
+    )
   cs_cols <- get_column_names(calib_summary, select = select_quo, n_reqs = list(select = "*"))
 
   # check if any summary should be included
   if (length(cs_cols$select) > 0) {
-    calib_summary <- gather(calib_summary, term, estimate, !!!cols_to_quos(cs_cols$select))
+    calib_summary <- calib_summary %>%
+      rename(!!!map(cs_cols$select, sym)) %>%
+      gather(term, estimate, !!!map(names(cs_cols$select), sym))
     visualization_data <- bind_rows(calib_coefs, calib_summary)
   } else {
     visualization_data <- calib_coefs
   }
 
   # check column availability
-  vis_cols <- get_column_names(visualization_data, x = enquo(x), color = enquo(color), shape = enquo(shape), size = enquo(size),
-                              n_reqs = list(color = "?", shape = "?", size = "?"))
+  vis_quos <-
+    list(x = enquo(x), color = enquo(color), shape = enquo(shape), size = enquo(size))
 
   # make sure we only plot unique data sets
   visualization_data <- unique(visualization_data)
@@ -104,94 +121,22 @@ iso_visualize_delta_calib_fits <- function(dt, x = default(file_datetime), color
     mutate(term = as_factor(term)) %>%
     filter(!is.na(estimate)) %>%
     ggplot() +
-    aes_q(x = sym(vis_cols$x), y = sym("estimate"),
-          color = if(!is_empty(vis_cols$color)) sym(vis_cols$color) else NULL,
-          shape = if(!is_empty(vis_cols$shape)) sym(vis_cols$shape) else NULL,
-          size = if(!is_empty(vis_cols$size)) sym(vis_cols$size) else NULL
-        ) +
-    geom_errorbar(data = function(df) filter(df,!is.na(std.error)), map = aes(ymin = estimate - std.error, ymax = estimate + std.error), width = 0, size = 1) +
-    { if (is_empty(vis_cols$size)) geom_point(size = 4) else geom_point() } +
-    facet_wrap(~term, ncol = 1, scales = "free_y") +
-    theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-    theme(plot.margin = margin(10, 5, 15, 20)) +
-    labs(y="")
-
-  # type of x axis
-  if (is(visualization_data[[vis_cols$x]], "POSIXct")) {
-    p <- p +
-      scale_x_datetime(date_breaks = date_breaks, date_labels = date_labels) +
-      labs(x = "")
-  }
-
-  return(p)
-}
-
-#' Visualize results for delta calibration
-#' @inheritParams iso_print_data_table
-#' @param x the x axis for the calibration parameters, by default a datetime but can also work as text
-#' @param include_from_summary which parameters from the fit summary to include
-#' @param date_breaks what breaks to use for the x axis if it is a datetime
-#' @param date_labels datetime label pattern for x axis if it is a datetime
-#' @note combine with a single iso_visualize_calib_fits?? or at least simplifty both functions!
-#' @export
-iso_visualize_area_calib_fits <- function(dt, x = default(file_datetime), color = NULL, shape = NULL, size = NULL,
-                                           date_breaks = "2 hours", date_labels = "%d %b %H:%M",
-                                           include_from_summary = c(adj.r.squared, deviance)) {
-
-  # safety checks
-  if (missing(dt)) stop("no data table supplied", call. = FALSE)
-  if (length(missing <- setdiff(c("calib_area_coefs", "calib_area_summary"), names(dt))) > 0) {
-    glue("missing columns in data table: '{collapse(missing, \"', '\")}'. Make sure to run iso_calibrate_area() first and keep these columns.") %>%
-      stop(call. = FALSE)
-  }
-
-  # pull out all coefficients (all for now, should always show all)
-  calib_coefs <- dt %>% select(-calib_area_summary) %>%
-    iso_unnest_area_calib_coefs(select = c(term, estimate, std.error))
-
-  # pull out summary
-  select_quo <- enquo(include_from_summary)
-  calib_summary <- dt %>% select(-calib_area_fit, -calib_area_coefs) %>%
-    iso_unnest_area_calib_summary(select = !!select_quo)
-  cs_cols <- get_column_names(calib_summary, select = select_quo, n_reqs = list(select = "*"))
-
-  # check if any summary should be included
-  if (length(cs_cols$select) > 0) {
-    calib_summary <- gather(calib_summary, term, estimate, !!!cols_to_quos(cs_cols$select))
-    visualization_data <- bind_rows(calib_coefs, calib_summary)
-  } else {
-    visualization_data <- calib_coefs
-  }
-
-  # FIXME: after this it is all the same as in the delta calibration!! -----
-
-  # check column availability
-  vis_cols <- get_column_names(visualization_data, x = enquo(x), color = enquo(color), shape = enquo(shape), size = enquo(size),
-                               n_reqs = list(color = "?", shape = "?", size = "?"))
-
-  # make sure we only plot unique data sets
-  visualization_data <- unique(visualization_data)
-
-  # generate plot
-  p <- visualization_data %>%
-    # make sure term factor is in order (i.e. paneling)
-    mutate(term = as_factor(term)) %>%
-    filter(!is.na(estimate)) %>%
-    ggplot() +
-    aes_q(x = sym(vis_cols$x), y = sym("estimate"),
-          color = if(!is_empty(vis_cols$color)) sym(vis_cols$color) else NULL,
-          shape = if(!is_empty(vis_cols$shape)) sym(vis_cols$shape) else NULL,
-          size = if(!is_empty(vis_cols$size)) sym(vis_cols$size) else NULL
+    aes(x = !!vis_quos$x, y = estimate,
+        color = !!if(!quo_is_null(vis_quos$color)) vis_quos$color else quo(),
+        shape = !!if(!quo_is_null(vis_quos$shape)) vis_quos$shape else quo(),
+        size = !!if(!quo_is_null(vis_quos$size)) vis_quos$size else quo()
     ) +
-    geom_errorbar(data = function(df) filter(df,!is.na(std.error)), map = aes(ymin = estimate - std.error, ymax = estimate + std.error), width = 0, size = 1) +
-    { if (is_empty(vis_cols$size)) geom_point(size = 4) else geom_point() } +
+    geom_errorbar(data = function(df) filter(df,!is.na(std.error)),
+                  map = aes(ymin = estimate - std.error, ymax = estimate + std.error), width = 0, size = 1) +
+    { if (quo_is_null(vis_quos$size)) geom_point(size = 4) else geom_point() } +
     facet_wrap(~term, ncol = 1, scales = "free_y") +
     theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
     theme(plot.margin = margin(10, 5, 15, 20)) +
     labs(y="")
 
   # type of x axis
-  if (is(visualization_data[[vis_cols$x]], "POSIXct")) {
+  x <- mutate(visualization_data, x = !!vis_quos$x)$x
+  if (is(x, "POSIXct")) {
     p <- p +
       scale_x_datetime(date_breaks = date_breaks, date_labels = date_labels) +
       labs(x = "")
@@ -200,26 +145,31 @@ iso_visualize_area_calib_fits <- function(dt, x = default(file_datetime), color 
   return(p)
 }
 
+#@ implement me
+iso_plot_calibration_range <- function() {
 
+}
 
 #' Visualize the data
 #'
 #' General purpose visualization function.
 #'
 #' @inheritParams iso_map_peaks
-#' @inheritParams iso_visualize_delta_calib_fits
-#' @param y which columns to visualize
+#' @inheritParams iso_visualize_calibration_parameters
+#' @param y which columns to visualize, combine with c()
 #' @param group what to group by, multiple columns allowed (combine with c(...)), usually not necessary if color or other grouping is defined
 #' @param lines whether to plot lines
 #' @param points whether to plot points
 #' @export
 #' @note should probably make sure that the default columns for gather 'panel' and 'value' do not exist...
 #' @note it would be great to allow renaming of the columns via this (especially the y column)
+#' @export
 iso_visualize_data <- function(dt, x, y, group = NULL, color = NULL, shape = NULL, size = NULL, linetype = NULL, lines = TRUE, points = FALSE) {
   # safety checks
   if (missing(dt)) stop("no data table supplied", call. = FALSE)
   if (missing(x)) stop("have to provide an x to plot", call. = FALSE)
   if (missing(y)) stop("have to provide at least one column to plot", call. = FALSE)
+  #Fixme, continue here
   vis_cols <- get_column_names(!!enquo(dt), x = enquo(x), y = enquo(y), group = enquo(group),
                               color = enquo(color), shape = enquo(shape), linetype = enquo(linetype), size = enquo(size),
                               n_reqs = list(y = "+", group = "*", color = "?", shape = "?", linetype = "?", size = "?"))
