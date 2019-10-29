b2aa5848f7db6655e8c28e1f86beccd2b1eb8799
Sebastian Kopf <sebastian.kopf@colorado.edu>
2019-10-28 21:24:26 -0600

    peak calculations outline fo functions

---
+++ R/peak_calculations.R
@@ -0,0 +1,38 @@
+# integrate peaks =====
+
+# should be an S3 method that works for iso_files and combination of raw_data and peak_table
+# calculates peak areas, peak heights, backgrounds, etc.
+
+#' Integrate peak table peaks
+#'
+#' Not yet implemented.
+#'
+#' @export
+iso_integrate_peaks <- function(...) {
+  stop("sorry, not yet implemented", call. = FALSE)
+}
+
+# calculate peak ratios ====
+
+# should be S3 method for iso_files and peak_table data frames
+
+#' Calculate peak ratios
+#'
+#' Not yet implemented.
+#'
+#' @export
+iso_calculate_peak_ratios <- function(...) {
+  stop("sorry, not yet implemented", call. = FALSE)
+}
+
+# calculate peak deltas =====
+
+#' Calculate peak deltas
+#'
+#' Not yet implemented
+#'
+#' @export
+iso_calculate_peak_delats <- function(...) {
+  stop("sorry, not yet implemented", call. = FALSE)
+}
+
