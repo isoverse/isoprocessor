56665972afa59570448ef219b08efcf7282bac27
Sebastian Kopf <sebastian.kopf@colorado.edu>
2019-11-04 09:01:00 -0700

    update runtime

--- binder/install.R
+++ binder/install.R
@@ -1,6 +1,5 @@
 # Package itself with all dependencies (including suggested packages)
 install.packages("devtools")
-install.packages("tidyr") # manual install to get newer version
 devtools::install_github("isoverse/isoreader", ref = "binder", dependencies = TRUE)
 devtools::install_github("isoverse/isoprocessor", ref = "binder", dependencies = TRUE)
 
