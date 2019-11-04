0b8aece862ffa8defb7ae13f121cd0bd113edd69
Sebastian Kopf <sebastian.kopf@colorado.edu>
2019-11-04 07:50:37 -0700

    update binder install

--- binder/install.R
+++ binder/install.R
@@ -1,7 +1,7 @@
 # Package itself with all dependencies (including suggested packages)
 install.packages("devtools")
-devtools::install_github("KopfLab/isoreader", ref = "binder", dependencies = TRUE)
-devtools::install_github("KopfLab/isoprocessor", ref = "binder", dependencies = TRUE)
+devtools::install_github("isoverse/isoreader", ref = "binder", dependencies = TRUE)
+devtools::install_github("isoverse/isoprocessor", ref = "binder", dependencies = TRUE)
 
 # Packages for knitting
-install.packages(c("rmarkdown", "caTools", "bitops")) 
+install.packages(c("rmarkdown", "caTools", "bitops"))
