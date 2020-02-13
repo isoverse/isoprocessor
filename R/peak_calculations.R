cfb426176594c7330f95429a43674a2e269531c5
Sebastian Kopf <sebastian.kopf@colorado.edu>
2020-02-13 01:43:45 -0700

    move check expressions to isoprocessor

--- R/peak_calculations.R
+++ R/peak_calculations.R
@@ -28,7 +28,7 @@ iso_combine_raw_data_with_peak_table <- function(
   raw_data_cols <- get_column_names(
     raw_data, file_id = enquo(file_id), n_reqs = list(file_id = "+"))
   data_trace_quo <- enquo(data_trace)
-  isoreader:::check_expressions(raw_data, data_trace_quo)
+  check_expressions(raw_data, data_trace_quo)
   if (rlang::quo_is_null(data_trace_quo)) data_trace_quo <- quo("all")
 
   # find peak table columns
