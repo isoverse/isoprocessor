c8f64afa7025bbf5c6894194bb3fd38486e0a072
Sebastian Kopf <sebkopf>
2020-09-28 12:10:56 -0600

    several bug fixes
    multi model tests
    checking for mutate error by error type
    including nobs column in model summary

--- tests/testthat/test-calibration.R
+++ tests/testthat/test-calibration.R
@@ -79,26 +79,49 @@ test_that("test default behavior of calibrations", {
     df %>% iso_generate_calibration(model = lm(hwy ~ cty), is_standard = TRUE),
     "parameter.*was renamed to.*use_in_calib"
   )
+  # generate single model calibration for testing
   expect_message(
     df_calib <- df %>% iso_generate_calibration(model = lm(hwy ~ cty), use_in_calib = TRUE),
     "generating calibration.*1 model.*2 data group.*filter \'TRUE\'.*new column \'resid\'.*new column \'in_calib\'"
   )
+  # generate multi model calibration for testing
+  expect_message(
+    df_calibs <- df %>% iso_generate_calibration(model = c(m1 = lm(hwy ~ cty), m2 = lm(hwy ~ cty^2)), use_in_calib = TRUE),
+    "generating calibration.*2 models.*2 data group.*filter \'TRUE\'.*new column \'resid\'.*new column \'in_calib\'"
+  )
   expect_equal(df_calib %>% all_calibrations(), "")
+  expect_equal(df_calibs %>% all_calibrations(), "")
   expect_equal(df_calib %>% last_calibration(), "")
+  expect_equal(df_calibs %>% last_calibration(), "")
   expect_true(has_regression_fit(df_calib))
+  expect_true(has_regression_fit(df_calibs))
   expect_false(has_regression_fit(strip_regression_fits(df_calib)))
+  expect_false(has_regression_fit(strip_regression_fits(df_calibs)))
   expect_error(df_calib %>% iso_generate_calibration(model = lm(cty ~ hwy)), "already has an unnamed calibration")
+  expect_error(df_calibs %>% iso_generate_calibration(model = lm(cty ~ hwy)), "already has an unnamed calibration")
   expect_message(
     df_calib2 <- df_calib %>% strip_regression_fits() %>% iso_generate_calibration(model = lm(cty ~ hwy), calibration = "x", use_in_calib = TRUE),
     "generating \'x\' calibration.*1 model.*2 data group.*filter \'TRUE\'.*new column \'x_resid\'.*new column \'x_in_calib\'"
   )
+  expect_message(
+    df_calibs2 <- df_calibs %>% strip_regression_fits() %>% iso_generate_calibration(model = lm(cty ~ hwy), calibration = "x", use_in_calib = TRUE),
+    "generating \'x\' calibration.*1 model.*4 data group.*filter \'TRUE\'.*new column \'x_resid\'.*new column \'x_in_calib\'"
+  )
+
   expect_equal(df_calib2 %>% all_calibrations(), c("", "x"))
+  expect_equal(df_calibs2 %>% all_calibrations(), c("", "x"))
   expect_equal(df_calib2 %>% last_calibration(), "x")
+  expect_equal(df_calibs2 %>% last_calibration(), "x")
   expect_true(has_regression_fit(df_calib2))
+  expect_true(has_regression_fit(df_calibs2))
   expect_false(has_regression_fit(df_calib2, calibration = ""))
+  expect_false(has_regression_fit(df_calibs2, calibration = ""))
   expect_true(has_regression_fit(df_calib2, calibration = "x"))
+  expect_true(has_regression_fit(df_calibs2, calibration = "x"))
   expect_false(has_regression_fit(strip_regression_fits(df_calib2), calibration = "x"))
+  expect_false(has_regression_fit(strip_regression_fits(df_calibs2), calibration = "x"))
   expect_error(df_calib2 %>% iso_generate_calibration(model = lm(cty ~ hwy), calibration = "x"), "already has a calibration name.*x")
+  expect_error(df_calibs2 %>% iso_generate_calibration(model = lm(cty ~ hwy), calibration = "x"), "already has a calibration name.*x")
 
   # loess
   expect_equal(correct_loess_date_time(df, quos(lm(y ~ datetime))), quos(lm(y ~ datetime)))
@@ -111,20 +134,32 @@ test_that("test default behavior of calibrations", {
   expect_warning(df %>% iso_generate_calibration(model = loess(cty ~ hwy), use_in_calib = TRUE, quiet = TRUE), "discouraged")
   expect_silent(df %>% iso_generate_calibration(model = loess(cty ~ hwy), calibration = "drift", use_in_calib = TRUE, quiet = TRUE))
 
-  # applying model
+  # applying model calibrations
   expect_error(iso_apply_calibration(df_calib), "no variable.*specified")
-  expect_message(iso_apply_calibration(df_calib, hwy), "applying calibration to infer.*hwy.*2 data group.*resulting value.*hwy_pred")
+  expect_error(iso_apply_calibration(df_calibs), "no variable.*specified")
+  expect_message(iso_apply_calibration(df_calib, hwy), "applying calibration to infer.*hwy.*2 data group.*in 1 model.*resulting value.*hwy_pred")
+  expect_message(iso_apply_calibration(df_calibs, hwy), "applying calibration to infer.*hwy.*2 data group.*in 2 model.*resulting value.*hwy_pred")
   expect_message(iso_apply_calibration(df_calib, hwy, calculate_error = TRUE), "applying calibration to infer.*hwy.*2 data group.*resulting value.*hwy_pred.*estimated error.*hwy_pred_se")
-  expect_message(iso_apply_calibration(df_calib, cty), "applying calibration to infer.*cty.*2 data group.*resulting value.*cty_pred")
-  expect_message(calib_applied <- iso_apply_calibration(df_calib2, cty, calculate_error = TRUE), "applying.*x.*calibration to infer.*cty.*2 data group.*resulting value.*cty_pred.*estimated error.*cty_pred_se")
+  expect_message(iso_apply_calibration(df_calibs, hwy, calculate_error = TRUE), "applying calibration to infer.*hwy.*2 data group.*resulting value.*hwy_pred.*estimated error.*hwy_pred_se")
+  expect_message(iso_apply_calibration(df_calib, cty), "applying calibration to infer.*cty.*2 data group.*1 model.*resulting value.*cty_pred")
+  expect_message(iso_apply_calibration(df_calibs, cty), "applying calibration to infer.*cty.*2 data group.*.*2 model.*resulting value.*cty_pred")
+
+  # save applied calibs for single model from 'x' calibration
+  expect_message(calib_applied <- iso_apply_calibration(df_calib2, cty, calculate_error = TRUE), "applying.*x.*calibration to infer.*cty.*2 data group.*1 model.*resulting value.*cty_pred.*estimated error.*cty_pred_se")
+  # save applied calibs for multi model from the default ('') calibration
+  expect_message(calibs_applied <- iso_apply_calibration(df_calibs, cty, calculate_error = TRUE), "applying calibration to infer.*cty.*2 data group.*2 model.*resulting value.*cty_pred.*estimated error.*cty_pred_se")
   expect_error(df_calib %>% strip_regression_fits() %>% iso_apply_calibration(hwy), "regression fits.*no longer available")
+  expect_error(df_calibs %>% strip_regression_fits() %>% iso_apply_calibration(hwy), "regression fits.*no longer available")
 
   # range
-  expect_error(calib_applied %>% iso_evaluate_calibration_range(DNE), "not.*valid")
+  expect_error(calib_applied %>% iso_evaluate_calibration_range(DNE), "not.*valid", class = "dplyr:::mutate_error")
+  expect_error(calibs_applied %>% iso_evaluate_calibration_range(DNE), "not.*valid", class = "dplyr:::mutate_error")
   expect_message(calib_range <- calib_applied %>% iso_evaluate_calibration_range(cty, hwy, cty*hwy),
-                 "evaluating range for terms.*cty.*hwy.*cty \\* hwy.*x.*calibration for 2 data group.*new column.*in_range")
+                 "evaluating range for terms.*cty.*hwy.*cty \\* hwy.*x.*calibration for 2 data group.*1 model.*new column.*in_range")
+  expect_message(calibs_range <- calibs_applied %>% iso_evaluate_calibration_range(cty, hwy, cty*hwy),
+                 "evaluating range for terms.*cty.*hwy.*cty \\* hwy.*calibration for 2 data group.*2 model.*new column.*in_range")
 
-  # get data
+  # get data (single model)
   expect_warning(calib_range %>% iso_get_calibration_data(keep_remaining_nested_data = TRUE), "renamed")
   expect_warning(calib_range %>% iso_get_calibration_data(keep_other_list_data = TRUE), "renamed")
   expect_message(out <- calib_range %>% iso_get_calibration_data(), "retrieving all data")
@@ -158,7 +193,10 @@ test_that("test default behavior of calibrations", {
     c("model_fit", "model_coefs", "model_summary", "model_range")
   )
 
-  # get coefficients
+  # get data (multiple models)
+  expect_message(outs <- calibs_range %>% iso_get_calibration_data(), "retrieving all data")
+
+  # get coefficients (single model)
   expect_warning(calib_range %>% iso_get_calibration_coefficients(keep_remaining_nested_data = TRUE), "removed")
   expect_warning(calib_range %>% iso_get_calibration_coefficients(keep_other_list_data = TRUE), "renamed")
   expect_warning(calib_range %>% iso_get_calibration_coefficients(keep_calibration_regressions = TRUE), "cannot.*without.*parameters")
@@ -181,7 +219,13 @@ test_that("test default behavior of calibrations", {
     c("year", "calib", "calib_ok", "calib_points", "x_calib", "x_calib_ok", "x_calib_points", "term", "estimate")
   )
 
-  # get summary
+  # get coefficients (multiple models)
+  expect_message(
+    outs <- calibs_range %>% iso_get_calibration_coefficients(),
+    "retrieving all coefficient.*calibration"
+  )
+
+  # get summary (single model)
   expect_warning(calib_range %>% iso_get_calibration_summary(keep_remaining_nested_data = TRUE), "removed")
   expect_warning(calib_range %>% iso_get_calibration_summary(keep_other_list_data = TRUE), "renamed")
   expect_warning(calib_range %>% iso_get_calibration_summary(keep_calibration_regressions = TRUE), "cannot.*without.*parameters")
@@ -191,7 +235,7 @@ test_that("test default behavior of calibrations", {
   )
   expect_equal(
     names(out),
-    c("year", "x_calib", "x_calib_ok", "x_calib_points", "r.squared", "adj.r.squared", "sigma", "statistic", "p.value", "df", "logLik", "AIC", "BIC", "deviance", "df.residual"))
+    c("year", "x_calib", "x_calib_ok", "x_calib_points", "r.squared", "adj.r.squared", "sigma", "statistic", "p.value", "df", "logLik", "AIC", "BIC", "deviance", "df.residual", "nobs"))
   expect_message(
     out <- calib_range %>% iso_get_calibration_summary(select = c(r.squared, RSD = sigma)),
     "retrieving.*summary column.*r.squared.*RSD = sigma.*x.*calibration"
@@ -206,7 +250,13 @@ test_that("test default behavior of calibrations", {
     c("year", "calib", "calib_ok", "calib_points", "x_calib", "x_calib_ok", "x_calib_points", "r.squared", "sigma")
   )
 
-  # get range
+  # get summary (multiple models)
+  expect_message(
+    out <- calibs_range %>% iso_get_calibration_summary(),
+    "retrieving all summary.*calibration"
+  )
+
+  # get range (single model)
   expect_true(calib_range %>% has_model_range())
   expect_false(calib_range %>% has_model_range(calibration = ""))
   expect_warning(calib_range %>% iso_get_calibration_range(keep_remaining_nested_data = TRUE), "removed")
@@ -234,11 +284,23 @@ test_that("test default behavior of calibrations", {
     c("year", "calib", "calib_ok", "calib_points", "x_calib", "x_calib_ok", "x_calib_points", "term", "units")
   )
 
-  # get parameters
-  expect_warning(calib_range %>% iso_unnest_calibration_parameters(), "conflicting name")
+  # get range (multiple models)
+  expect_message(
+    outs <- calibs_range %>% iso_get_calibration_range(),
+    "retrieving all calibration range.*calibration"
+  )
+
+  # get parameters (single model)
+  expect_warning(calib_range %>% iso_get_calibration_parameters(), "conflicting name")
   expect_equal(
-    suppressWarnings(calib_range %>% iso_unnest_calibration_parameters(select_from_coefs = c(term, estimate), select_from_summary = c(sigma))) %>% names(),
+    calib_range %>% iso_get_calibration_parameters(select_from_coefs = c(term, estimate), select_from_summary = c(sigma)) %>% names(),
     c("year", "x_calib", "x_calib_ok", "x_calib_points", "term", "estimate", "sigma")
+  )
+
+  # get parameters (multiople models)
+  expect_equal(
+    calibs_range %>% iso_get_calibration_parameters(select_from_coefs = c(term, estimate), select_from_summary = c(sigma)) %>% names(),
+    c("year", "calib", "calib_ok", "calib_points", "term", "estimate", "sigma")
   )
 
 
